2401bd86a318208bebb4bae802131424f545f214
Michał Krzysztof Feiler <m@mikf.pl>
2020-06-16 06:00:40 +0200

    lots of javadoc. also, removed the size and isEmpty from Store

--- src/pl/mikf/uam/pob/peselset/Pesel.java
+++ src/pl/mikf/uam/pob/peselset/Pesel.java
@@ -6,6 +6,10 @@ public class Pesel {
 
     private final char[] c;
 
+    /**
+     * @param s -- a string of 11 decimal digits being a PESEL
+     * @throws BadPesel if the PESEL checksum is wrong or if month is illegal or day is too high
+     */
     public Pesel(String s) throws BadPesel {
         c = s.toCharArray();
         if (c.length != 11)
@@ -18,6 +22,9 @@ public class Pesel {
             throw new BadPesel();
     }
 
+    /**
+     * @return simply returns a String of 11 decimal digits and nothing else, as on input
+     */
     @Override
     public String toString() {
         return String.valueOf(c);
@@ -29,6 +36,10 @@ public class Pesel {
     public static class BadChecksum extends BadPesel {
     }
 
+    /**
+     * @param o -- the other object
+     * @return true iff same class and not a subclass and same chars of pesel
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -39,16 +50,27 @@ public class Pesel {
         return Arrays.equals(c, pesel.c);
     }
 
+    /**
+     * @return just Arrays.hashCode of the chars array of PESEL
+     */
     @Override
     public int hashCode() {
         return Arrays.hashCode(c);
     }
 
+    /**
+     * @param c -- a character supposed to be an ASCII code for a digit
+     * @return whether it is a decimal digit. also has an assertion for whether char 1 is right after char 0.
+     */
     public static boolean isDigit(char c) {
         assert '0' == '1' - 1;
         return c >= '0' && c <= '9';
     }
 
+    /**
+     * @param c -- a dec digit ASCII char code
+     * @return the integer value of that digit
+     */
     public static int value(char c) {
         if (!isDigit(c))
             throw new IllegalArgumentException(
@@ -56,24 +78,47 @@ public class Pesel {
         return c - '0';
     }
 
+    /**
+     * @param x -- index (from zero) of pesel digits
+     * @return the integer value of the digit under index
+     */
     public int v(int x) {
         return value(c[x]);
     }
 
-
+    /**
+     * @param x -- an index between 0 and 9, incl
+     * @return x'th digit of pesel indexed from zero multiplied by ten plus x'th digit of pesel indexed from one
+     */
     public int vv(int x) {
         return v(x) * 10 + v(x + 1);
     }
 
+    /**
+     * @param x an index between 0 and 9, incl
+     * @param y an index one greater than x
+     * @return after an assertion that y is one greater than x, (zero-indexed) x'th digit times 10 plus y'th times 1
+     */
     public int v(int x, int y) {
         assert y == x + 1;
         return vv(x);
     }
 
+    /**
+     * @param x instead of numbering the 11 digits of pesel with numbers,
+     *          we numerate them with chars a-k like wikipedia
+     * @return the digit of pesel under the letter-index, as integer
+     */
     public int v(char x) {
         return v(x - 'a');
     }
 
+    /**
+     * numerating the 11 digits of pesel with chars a-k.
+     * @param x one less than y
+     * @param y one greater than x
+     * @return the digit for x times 10 plus the digit for y times one
+     */
     public int v(char x, char y) {
         return v(x - 'a', y - 'a');
     }
@@ -82,25 +127,41 @@ public class Pesel {
         return i % 2 == 0;
     }
 
+    /**
+     * @return whether the month is valid, regardless of the mod20 century mark
+     */
     public boolean monthCenturyCheck() {
         return even(v('c')) ? v('d') != 0 : v('d') <= 2;
     }
 
+    /**
+     * @return the century as one of 1800, 1900, 2000, 2100, 2200
+     */
     public int century() {
         int r = v('a') / 2;
         if (r == 4) r = -1;
         return r * 100 + 1900;
     }
 
+    /**
+     * @return the year of birth of the pesel-owner, modulo 100 (like, just the 98 from 1998)
+     */
     public int centurysYear() {
         return v('a') % 2 * 10 + v('b');
     }
 
+
+    /**
+     * @return whether the birth year was leap. takes into account the 4, the 100, and the 400.
+     */
     public boolean isLeapYear() {
         int y = centurysYear();
         return y % 4 == 0 && (y != 0 || v('a') / 2 == 1);
     }
 
+    /**
+     * @return how many days did the month of birth have (in the year of birth -- Feb in leap/common)
+     */
     public int monthDaysCount() {
         switch (v('c', 'd') % 20) {
             case 2:
@@ -118,6 +179,9 @@ public class Pesel {
         }
     }
 
+    /**
+     * @return whether the day is not too high for the month (and for the year -- Feb in leap/common)
+     */
     public boolean dayCheck() {
         int d = v('e', 'f');
         return d > 0 && d <= monthDaysCount();
@@ -126,6 +190,9 @@ public class Pesel {
     public static final int[] FOR_A = {9, 7, 3, 1, 9, 7, 3, 1, 9, 7};
     public static final int[] FOR_B = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1};
 
+    /**
+     * @return checking the checksum digit by calculating the correct one and comparing
+     */
     public boolean checkSumMethodA() {
         int acc = -v('k');
         for (int i = 0; i < 10; i++)
@@ -133,6 +200,9 @@ public class Pesel {
         return acc == 0;
     }
 
+    /**
+     * @return checking the checksum digit by having the accumulator render a modulo zero after adding the checksum digit
+     */
     public boolean checkSumMethodB() {
         int acc = 0;
         for (int i = 0; i < 11; i++)
--- src/pl/mikf/uam/pob/peselset/Store.java
+++ src/pl/mikf/uam/pob/peselset/Store.java
@@ -8,31 +8,29 @@ import java.util.Vector;
 public class Store implements Iterable<Store.Entry> {
     private Vector<Entry> vec = new Vector<>();
 
-    public int size() {
-        return vec.size();
-    }
-
-    public boolean isEmpty() {
-        return vec.isEmpty();
-    }
-
-    public boolean contains(Object o) {
-        for (Entry i : vec) {
-            if (i.pesel.equals(o)) return true;
-        }
-        return false;
-    }
-
+    /**
+     * replaces in-place if there is one with same pesel, appends to end if there is none
+     * @param entry -- the entry that we want to add
+     */
     public void add(Entry entry) {
         int i = vec.indexOf(entry);
         if (i == -1) vec.add(entry);
         else vec.setElementAt(entry, i);
     }
 
+    /**
+     * @return an iterator of all the entries in order of addition
+     */
     public Iterator<Entry> iterator() {
         return vec.iterator();
     }
 
+    /**
+     * writes the lines "pesel firstname lastname" to the outputstream, ending each with newline character
+     * will do it in order in which they were added
+     * @param writer -- an OutputStream, like FileOutputStream or ByteArrayOutputStream
+     * @throws IOException if the write method of the OS throws IOException
+     */
     public void write(OutputStream writer) throws IOException {
         for (Entry i : this) {
             writer.write((i.toString() + "\n").getBytes());
@@ -43,6 +41,9 @@ public class Store implements Iterable<Store.Entry> {
         public final Pesel pesel;
         public final String firstName, lastName;
 
+        /**
+         * a line was given to the Entry constructor that had not just three fields. could have been zero.
+         */
         public static class BadNumberOfFields extends Exception {
             int number;
 
@@ -51,6 +52,11 @@ public class Store implements Iterable<Store.Entry> {
             }
         }
 
+        /**
+         * @param line : a line of exactly three space-delimited substrings (pesel, firstname, lastname)
+         * @throws BadNumberOfFields if there is not three but zero or any other number
+         * @throws Pesel.BadPesel if there are three but the pesel is deemed invalid
+         */
         public Entry(String line) throws BadNumberOfFields, Pesel.BadPesel {
             String[] fields = line.split(" ");
             if (fields.length != 3)
@@ -60,11 +66,23 @@ public class Store implements Iterable<Store.Entry> {
             lastName = fields[2];
         }
 
+        /**
+         * @return returns the 11 digits of pesel followed by a space, the firstname, and the lastname.
+         * no final delimiting character like a newline or something.
+         */
         @Override
         public String toString() {
             return pesel + " " + firstName + " " + lastName;
         }
 
+        /**
+         * @param o -- another object to compare
+         * @return not really whether the objects are both equal entries
+         * but only whether both are entries and whether both have the same pesels.
+         * It came in handy for the use of indexOf on vec which was just neat.
+         * I only found a proper predicate-using method for removal and not for getting index.
+         * Yea I know I shouldn't have done that to an equals method.
+         */
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
@@ -75,6 +93,11 @@ public class Store implements Iterable<Store.Entry> {
             return pesel.equals(entry.pesel);
         }
 
+        /**
+         * @return not really the hashcode of pesel with names but the hashcode of just the pesel object.
+         * it came in handy to use indexOf on vec. See equals of this class
+         * not sure if indexOf even needed the hashCode method, but why not add it if there's equals.
+         */
         @Override
         public int hashCode() {
             return pesel.hashCode();
